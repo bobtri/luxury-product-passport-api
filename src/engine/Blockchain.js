cd54ec947275f0db30b81caf0974dfaa4399fc86
Bobby <bobtri15@gmail.com>
2026-08-28 18:41:08 +0200

    feat: validate product ownership before transfer

--- src/engine/Blockchain.js
+++ src/engine/Blockchain.js
@@ -14,8 +14,39 @@ class Blockchain {
     return this.chain[this.chain.length - 1];
   }
 
+  getCurrentOwner(serialNumber) {
+    const transactions = [];
+
+    for (const block of this.chain) {
+      for (const transaction of block.data) {
+        if (transaction.serialNumber === serialNumber) {
+          transactions.push(transaction);
+        }
+      }
+    }
+
+    for (const transaction of this.pendingTransactions) {
+      if (transaction.serialNumber === serialNumber) {
+        transactions.push(transaction);
+      }
+    }
+
+    if (transactions.length === 0) {
+      return null;
+    }
+
+    return transactions[transactions.length - 1].toAddress;
+  }
+
   addTransaction(transaction) {
+    const currentOwner = this.getCurrentOwner(transaction.serialNumber);
+
+    if (currentOwner !== null && transaction.fromAddress !== currentOwner) {
+      throw new Error('Transaction rejected: sender is not the current owner');
+    }
+
     this.pendingTransactions.push(transaction);
+
     return transaction;
   }
 }
