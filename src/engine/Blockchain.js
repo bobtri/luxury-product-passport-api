0d367982aa38e76c2c4aca6a3c8c62ce7742e6c6
Bobby <bobtri15@gmail.com>
2026-08-30 18:09:17 +0200

    feat: validate blockchain integrity

--- src/engine/Blockchain.js
+++ src/engine/Blockchain.js
@@ -68,6 +68,23 @@ class Blockchain {
 
     return newBlock;
   }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
 
 export default Blockchain;
